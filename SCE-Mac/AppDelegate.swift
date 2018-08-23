61155b2d368739355bfcce24a585aa4e2ce450b3
Ronald Mannak <ronaldmannak@me.com>
2018-08-22 23:45:27 -0700

    Added empty preferences window

--- SCE-Mac/AppDelegate.swift
+++ SCE-Mac/AppDelegate.swift
@@ -11,7 +11,7 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    var preferencesController: PreferencesWindowController? = nil
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
@@ -21,6 +21,18 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
-
+    
+    @IBAction func showPreferences(_ sender: Any) {
+        
+        if preferencesController == nil {
+            let storyboard = NSStoryboard(name: NSStoryboard.Name(rawValue: "Preferences"), bundle: nil)
+            preferencesController = storyboard.instantiateInitialController() as? PreferencesWindowController
+        }
+        
+        if let preferencesController = preferencesController {
+            preferencesController.showWindow(sender)
+        }
+    }
+    
 }
 
