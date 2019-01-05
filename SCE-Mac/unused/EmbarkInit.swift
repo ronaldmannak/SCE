ed5df0ad074b47b92fb01a127272d7695ac18497
Ronald Mannak <ronaldmannak@me.com>
2019-01-04 23:37:57 -0800

    Code Cleanup

--- SCE-Mac/unused/EmbarkInit.swift
+++
@@ -1,9 +0,0 @@
-//
-//  EmbarkInit.swift
-//  SCE
-//
-//  Created by Ronald "Danger" Mannak on 10/4/18.
-//  Copyright © 2018 A Puzzle A Day. All rights reserved.
-//
-
-import Foundation
--- SCE-Mac/unused/EtherlimeRun.command
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd ${1}
-echo '$ etherlime deploy'
-etherlime deploy
-echo 'Done.'
