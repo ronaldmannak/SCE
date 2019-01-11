a626f9438157c37e773127cdb33acae283c58f8a
Ronald Mannak <ronaldmannak@me.com>
2019-01-10 23:15:11 -0800

    Code cleanup

--- Pods/Target Support Files/SavannaKit-iOS/SavannaKit-iOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SavannaKit_iOS : NSObject
-@end
-@implementation PodsDummy_SavannaKit_iOS
-@end
--- Pods/Target Support Files/SourceEditor-macOS/SourceEditor-macOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SourceEditor_macOS : NSObject
-@end
-@implementation PodsDummy_SourceEditor_macOS
-@end
